ac5a6bb4ba456625ed20ef4bb24b1ec26065c123
Nikolaj Garro Throksø <nikolajgt@hotmail.com>
2023-01-18 10:31:44 +0100

    Add project files.

---
+++ TestOgSikkerhed/Data/MyRoleHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TestOgSikkerhed.Data;
+
+public class MyRoleHandler
+{
+    public async Task CreateUserRoles(string userEmail, string role, IServiceProvider provider)
+    {
+        var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = provider.GetRequiredService<UserManager<IdentityUser>>();
+
+        IdentityResult roleResult;
+
+        var userRoleCheck = await roleManager.RoleExistsAsync(role);
+        if (!userRoleCheck)
+            roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+
+        IdentityUser identityUser = await userManager.FindByEmailAsync(userEmail);
+        await userManager.AddToRoleAsync(identityUser, role);
+    }
+}
